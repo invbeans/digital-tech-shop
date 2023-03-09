6cafa365bcf3bba29c931749f891e7292a54c269
invbeans <vlada.3234.vasileva@gmail.com>
2023-03-09 19:18:05 +0400

    first user tables migrations

---
+++ server/migrations/20230309143654_create_user_profile_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("user_profile", table => {
+        table.integer("user").primary()
+        table.string("firstname").notNullable()
+        table.integer("points").notNullable()
+      })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("user_profile")
+};
---
+++ server/migrations/20230309145138_create_user_activity_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable("user_activity", table => {
+        table.string("phone_number").notNullable().unique()
+        table.string("hashed_password").notNullable()
+        table.date("birthday_date")
+        table.string("email").notNullable().unique()
+      })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable("user_activity")
+};
