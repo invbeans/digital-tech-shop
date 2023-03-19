2b765edd0026e6ea9555e62d8859b36e072776fc
invbeans <vlada.3234.vasileva@gmail.com>
2023-03-19 12:10:05 +0400

    other (empty models, will del half of migrations)

--- server/migrations/20230308142751_create_meta_user_table.js
+++ server/migrations/20230308142751_create_meta_user_table.js
@@ -4,8 +4,8 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable("meta_user", table => {
-        table.integer("user").primary().notNullable()
-        table.integer("role").notNullable()
+        table.integer("user").primary().notNullable().references("id").inTable("user")
+        table.integer("role").notNullable().references("id").inTable("role")
         table.string("phone_number").notNullable().unique()
         table.string("hashed_password").notNullable()
         table.date("birthday_date")
