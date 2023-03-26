75acba2fe001459a044693382c27f9da7edc5132
invbeans <vlada.3234.vasileva@gmail.com>
2023-03-26 22:35:40 +0400

    added pico and empty controllers

--- client/shop-client/src/app/app.module.ts
+++ client/shop-client/src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminPageModule } from './admin-page/admin-page.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandModule } from './brand/brand.module';
+import { ContentPageModule } from './content-page/content-page.module';
+import { DiscountsModule } from './discounts/discounts.module';
+import { MainCatModule } from './main-cat/main-cat.module';
+import { MainModule } from './main/main.module';
+import { MakeOrderModule } from './make-order/make-order.module';
+import { ManagerPageModule } from './manager-page/manager-page.module';
+import { ProductModule } from './product/product.module';
+import { ReturnModule } from './return/return.module';
 import { SharedModule } from './shared/shared.module';
+import { SubCatModule } from './sub-cat/sub-cat.module';
+import { TrackOrderModule } from './track-order/track-order.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +25,20 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    FormsModule,
+    SharedModule,
+    MainModule,
+    MainCatModule,
+    SubCatModule,
+    ProductModule,
+    DiscountsModule,
+    BrandModule,
+    MakeOrderModule,
+    TrackOrderModule,
+    ReturnModule,
+    AdminPageModule,
+    ManagerPageModule,
+    ContentPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
