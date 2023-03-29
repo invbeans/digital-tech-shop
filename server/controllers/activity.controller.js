9c0768d46c3cc0d1b08f9cbb3b8d58322a60c839
invbeans <vlada.3234.vasileva@gmail.com>
2023-03-29 19:32:20 +0400

    user activity controller

--- server/controllers/activity.controller.js
+++ server/controllers/activity.controller.js
@@ -1,5 +1,207 @@
-class activityController {
+const Review = require('../models/Review')
+const Question = require('../models/Question')
+const Answer = require('../models/Answer')
+const ReturnApplication = require('../models/ReturnApplication')
+const ReturnProduct = require('../models/ReturnProduct')
 
+class activityController {
+    // --------- review CRUD ----------
+    async createReview(req, res) {
+        const { user, product, points, date, text } = req.body
+        await Review.query()
+            .insert({ user, product, points, date, text })
+            .then(review => res.json(review))
+            .catch(err => res.json(err.message))
+    }
+
+    async updateReview(req, res) {
+        const id = req.params.id
+        const { user, product, points, date, text } = req.body
+        await Review.query()
+            .patchAndFetchById(id, {
+                user, product, points, date, text
+            })
+            .then(review => {
+                if (review === null) res.json("Такого отзыва нет")
+                else res.json(review)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async deleteReview(req, res) {
+        const id = req.params.id
+        await Review.query()
+            .deleteById(id)
+            .then(amount => {
+                if (amount == 0) res.json("Такого отзыва нет")
+                else res.json(`Отзыв с id = ${id} удалён`)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async getReviewByProduct(req, res) {
+        const product = req.params.id
+        await Review.query()
+            .select("*")
+            .where("product", "=", product)
+            .then(review => res.json(review))
+            .catch(err => res.json(err.message))
+    }
+
+    // --------- question CRUD ----------
+    async createQuestion(req, res) {
+        const { user, product, date, text } = req.body
+        await Question.query()
+            .insert({ user, product, date, text })
+            .then(question => res.json(question))
+            .catch(err => res.json(err.message))
+    }
+
+    async updateQuestion(req, res) {
+        const id = req.params.id
+        const { user, product, date, text } = req.body
+        await Question.query()
+            .patchAndFetchById(id, {
+                user, product, date, text
+            })
+            .then(question => {
+                if (question === null) res.json("Такого вопроса нет")
+                else res.json(question)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async deleteQuestion(req, res) {
+        const id = req.params.id
+        await Question.query()
+            .deleteById(id)
+            .then(amount => {
+                if (amount == 0) res.json("Такого вопроса нет")
+                else res.json(`Вопрос с id = ${id} удалён`)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async getQuestionByProduct(req, res) {
+        const product = req.params.id
+        await Question.query()
+            .select("*")
+            .where("product", "=", product)
+            .then(question => res.json(question))
+            .catch(err => res.json(err.message))
+    }
+
+    // --------- answer CRUD ----------
+    async createAnswer(req, res) {
+        const { question, user, date, text } = req.body
+        await Answer.query()
+            .insert({ question, user, date, text })
+            .then(answer => res.json(answer))
+            .catch(err => res.json(err.message))
+    }
+
+    async updateAnswer(req, res) {
+        const id = req.params.id
+        const { question, user, date, text } = req.body
+        await Answer.query()
+            .patchAndFetchById(id, {
+                question, user, date, text
+            })
+            .then(answer => {
+                if (answer === null) res.json("Такого ответа нет")
+                else res.json(answer)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async deleteAnswer(req, res) {
+        const id = req.params.id
+        await Answer.query()
+            .deleteById(id)
+            .then(amount => {
+                if (amount == 0) res.json("Такого ответа нет")
+                else res.json(`Ответ с id = ${id} удалён`)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async getAnswerByQuestion(req, res) {
+        const question = req.params.id
+        await Answer.query()
+            .select("*")
+            .where("question", "=", question)
+            .then(answer => res.json(answer))
+            .catch(err => res.json(err.message))
+    }
+
+    // --------- return application CRUD ----------
+    async createReturnApplication(req, res) {
+        const { date, order, text, approved } = req.body
+        await ReturnApplication.query()
+            .insert({ date, order, text, approved })
+            .then(returnApplication => res.json(returnApplication))
+            .catch(err => res.json(err.message))
+    }
+
+    async updateReturnApplication(req, res) {
+        const id = req.params.id
+        const { date, order, text, approved } = req.body
+        await ReturnApplication.query()
+            .patchAndFetchById(id, {
+                date, order, text, approved
+            })
+            .then(returnApplication => {
+                if (returnApplication === null) res.json("Такой заявки на возврат нет")
+                else res.json(returnApplication)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async deleteReturnApplication(req, res) {
+        const id = req.params.id
+        await ReturnApplication.query()
+            .deleteById(id)
+            .then(amount => {
+                if (amount == 0) res.json("Такой заявки на возврат нет")
+                else res.json(`Заявка на возврат с id = ${id} удалена`)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async getReturnApplications(req, res){
+        await ReturnApplication.query()
+        .then(returnApplication => res.json(returnApplication))
+        .catch(err => res.json(err.message))
+    }
+
+    // --------- return product CRUD ----------
+    async createReturnProduct(req, res) {
+        const { return_application, product, proper_quality } = req.body
+        await ReturnProduct.query()
+            .insert({ return_application, product, proper_quality })
+            .then(returnProduct => res.json(returnProduct))
+            .catch(err => res.json(err.message))
+    }
+
+    async deleteReturnProduct(req, res) {
+        const id = req.params.id
+        await ReturnProduct.query()
+            .deleteById(id)
+            .then(amount => {
+                if (amount == 0) res.json("Такого возвращаемого товара нет")
+                else res.json(`Возвращаемый товар с id = ${id} удалён`)
+            })
+            .catch(err => res.json(err.message))
+    }
+
+    async getReturnProductByReturnApplication(req, res) {
+        const return_application = req.params.id
+        await ReturnProduct.query()
+            .select("*")
+            .where("return_application", "=", return_application)
+            .then(returnProduct => res.json(returnProduct))
+            .catch(err => res.json(err.message))
+    }
 }
 
 module.exports = new activityController()
--- server/routes/activity.routes.js
+++ server/routes/activity.routes.js
@@ -2,6 +2,33 @@ const Router = require('express')
 const router = new Router()
 const controller = require('../controllers/activity.controller')
 
-//router.post('/review', controller.createReview)
+// --------- review CRUD ----------
+router.post('/review', controller.createReview)
+router.put('/review/:id', controller.updateReview)
+router.delete('/review/:id', controller.deleteReview)
+router.get('/review/by_product/:id', controller.getReviewByProduct)
+
+// --------- question CRUD ----------
+router.post('/question', controller.createQuestion)
+router.put('/question/:id', controller.updateQuestion)
+router.delete('/question/:id', controller.deleteQuestion)
+router.get('/question/by_product/:id', controller.getQuestionByProduct)
+
+// --------- answer CRUD ----------
+router.post('/answer', controller.createAnswer)
+router.put('/answer/:id', controller.updateAnswer)
+router.delete('/answer/:id', controller.deleteAnswer)
+router.get('/answer/by_question/:id', controller.getAnswerByQuestion)
+
+// --------- return application CRUD ----------
+router.post('/return_application', controller.createReturnApplication)
+router.put('/return_application/:id', controller.updateReturnApplication)
+router.delete('/return_application/:id', controller.deleteReturnApplication)
+router.get('/return_application', controller.getReturnApplications)
+
+// --------- return product CRUD ----------
+router.post('/return_product', controller.createReturnProduct)
+router.delete('/return_product/:id', controller.deleteReturnProduct)
+router.get('/return_product/by_return_application/:id', controller.getReturnProductByReturnApplication)
 
 module.exports = router
